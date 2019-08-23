489180f247db1972e705523230d13dca7420c9b4
Lucas Matheus Santos Andrade <32241181+lucasmatheussan@users.noreply.github.com>
2019-08-23 01:00:25 -0300

    Update RoboLinear

--- RoboLinear-Entrega01.py
+++ RoboLinear-Entrega01.py
@@ -6,11 +6,12 @@ class RL2():
         self.lista = [x for x in comandos]
 
     def Processamento(self):
+        lista_copia = self.lista[::]
         if ("f" in self.lista and "t" in self.lista):
-            for i in self.lista:
+            for i in teste:
                 if "t" == i and "f" in self.lista:
-                    self.lista.remove("t")
-                    self.lista.remove("f")
+                  self.lista.remove("t")
+                  self.lista.remove("f")
                 elif "t" == i and "f" not in self.lista:
                     break
         return len(self.lista)
@@ -20,7 +21,6 @@ entrada = input().lower()
 teste.setListaDeComandos(entrada)
 resultado = teste.Processamento()
 print("%d metro(s) de distância  entre a posição inicial e a posição final do robô RL2" % resultado)
-
 
 
 
